2ed3888748ea9567a1a716bd9577b36ff4da73f4
Manfred Riem <mriem@manorrock.com>
2020-10-09 12:46:11 -0600

    Add outbound SSL to GreeingController

--- greeting-service-netty/src/main/java/com/performance/greeting/GreetingController.java
+++ greeting-service-netty/src/main/java/com/performance/greeting/GreetingController.java
@@ -2,14 +2,60 @@
 // Licensed under the MIT License.
 package com.performance.greeting;
 
+import java.security.KeyStore;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class GreetingController {
 
+    @Bean
+    public RestTemplate restTemplate() throws Exception {
+        KeyStore ks = KeyStore.getInstance("AzureKeyVault");
+        SSLContext sslContext = SSLContexts.custom()
+                .loadTrustMaterial(ks, new TrustSelfSignedStrategy())
+                .build();
+
+        HostnameVerifier allowAll = (String hostName, SSLSession session) -> true;
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, allowAll);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory
+                = new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(httpClient);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        return restTemplate;
+    }
+
     @GetMapping("/hello")
     public String helloWorld() {
-        return "Greetings from WebFlux";
+        String result = "Cached hello from WebFlux";
+
+        try {
+            //
+            // note change the URL below to correspond to your own service
+            // hosted externally from Azure Spring Cloud.
+            //
+            result = restTemplate().getForObject("https://52.146.30.23:8443/", String.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return result;
     }
 }
